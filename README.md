0f1b1dd5ae746f503a9690fbfaf4dd899f14a272
yash255 <yash31khamdelwal@gmail.com>
2022-07-17 23:52:43 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+"# Chat_Room" 
+"# ChatRoom" 
+"# ChatRoom" 
+"# ChatRoom" 
---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,15 @@
+// var stompClient=null;
+// function connect(){
+//     let socket = new SockJS("/server1");
+//    stompClient= stompClient.over(socket);
+//    stompClient.connect({},function(frame){
+//     console.log("Connected: "+frame);
+//    })
+// }
+
+
+
+
+
+
+// $(document).ready
